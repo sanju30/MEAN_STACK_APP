fda10e7aea813995668224a29aebe3790446128f
sanju30 <33180707+sanju30@users.noreply.github.com>
2019-09-17 17:57:37 +0530

    Add files via upload

---
+++ src/app/contact.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Contact} from './contact';
+import { map } from "rxjs/operators";
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor(private http: Http) { }
+
+//retrieving contact service
+getContacts()
+{
+
+ return this.http.get('http://localhost:9000/api/contacts')
+  .pipe(map(res=>res.json()));
+}
+
+
+//add contact method
+addContact(newContact)
+{
+  var headers = new Headers();
+  headers.append('Contact.type','application/json');
+  return this.http.post('http://localhost:9000/api/contact',newContact,{headers:headers}) .pipe(map(res=>res.json()));
+}
+
+//delete cotact method 
+deleteContact(id)
+{
+  return this.http.delete('http://localhost:9000/api/contact/'+ id)
+  .pipe(map(res=>res.json()));
+}
+}
